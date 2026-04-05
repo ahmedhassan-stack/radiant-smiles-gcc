bb02bc73e6cf71213e521da75421aabc8ded3c58
ahmedhassan-stack <ahmed.hassan12309@gmail.com>
2026-04-06 01:13:57 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
+  server: {base: '/radiant-smiles-gcc
     host: "::",
     port: 8080,
     hmr: {
